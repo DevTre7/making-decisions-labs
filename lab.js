07060595f87f9d43e0c9435d81d435f6efa90208
Emmanuel Dada <Emmanuel.dada71@gmail.com>
2022-11-04 00:58:25 -0500

    worked problem 8 & 9

--- lab.js
+++ lab.js
@@ -132,25 +132,43 @@ let passingScore = 7;
 
 /*
   Create a while-loop that logs "Your score is not high enough" while the score variable above is below passingScore. 
-  In each iteration of the loop, increase the score by one. If your code is successful, you will see your logged statement 7 times.
+  In each iteration of the loop, increase the score by one. If your code is successful,
+   you will see your logged statement 7 times.
 */
 
-//CODE HERE
+score = 0;
+while(score < passingScore){
+  console.log("Your score is not high enough")
+  score++;
+}
 
 ////////// INTERMEDIATE PROBLEMS //////////
 
 ////////// PROBLEM 9 //////////
 /*
   Create a variable called 'changeMyMind' and set it equal to true. 
-  Check to see if changeMyMind is set to true or false, if it is true, change the status to false, if it is false, change the status to true.
+  Check to see if changeMyMind is set to true or false, if it is true, change the status to false, if it is false,
+   change the status to true.
 
 */
 
-//CODE HERE
+let changeMyMind = true;
+
+if(changeMyMind === true){
+   changeMyMind = false
+}
+else{
+  changeMyMind = true
+}
+console.log(changeMyMind);
 
 ////////// PROBLEM 10 //////////
-// Using the "not" operator (!), change the current value of changeMyMind to true (it should currently be set to false due to the if-else statement in Problem 9). After you change the value of changeMyMind, console.log it's new value (it should now read true).
 
+/* Using the "not" operator (!), change the current value of changeMyMind to true 
+(it should currently be set to false due to the if-else statement in Problem 9). After you change the value
+ of changeMyMind, console.log it's new value (it should now read true).
+
+*/
 //CODE HERE
 
 ////////// ADVANCED PROBLEMS //////////
@@ -159,5 +177,14 @@ let passingScore = 7;
 
 let z = 5;
 // Create a while loop that continues to run while z is greater than 0. Within the while loop, log a countdown from the value of z to 1. Once you have logged the countdown, make sure you decrement z by 1. If your code is running properly, you should see: 5,4,3,2,1,4,3,2,1,3,2,1,2,1,1.
+//put another loop within the while loop - DJ
 
+/*let z =5
+while(z > O){
+  for(let i = k; i > 0; i--){
+    console.log(i);
+  }
+}
+
+*/
 //CODE HERE
